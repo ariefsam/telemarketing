b72fb08964b514baba3b2387756d79452d6dc624
agung <agung@fsn.co.id>
2020-08-14 14:52:37 +0700

    MainLayout

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
+  <q-layout view="hHh LpR fFf">
     <q-header elevated>
       <q-toolbar>
         <q-btn
@@ -9,112 +9,121 @@
           icon="menu"
           aria-label="Menu"
           @click="leftDrawerOpen = !leftDrawerOpen"
-        />
-
+        />  
         <q-toolbar-title>Telemarketing App</q-toolbar-title>
-
-        <div>
-          {{userEmail}}
-          <q-btn :to="'/login'" v-show="!isLoggedIn">Login</q-btn>
+        <q-space />
+        <!--here profile icon-->
+        <div class="profile-menu">
+          <q-btn flat no-caps>
+            <q-avatar size="34px">
+              <img :src="user.profilePic" @error="onImageLoadFailure($event)"/>
+            </q-avatar>
+            <div class="name q-ml-sm">{{user.name}}</div>
+            <q-menu transition-show="jump-down" transition-hide="jump-up">
+              <div style="width: 240px">
+                <div class="row no-wrap items-center profile-info">
+                  <q-avatar size="34px">
+                    <img :src="user.profilePic" @error="onImageLoadFailure($event)"/>
+                  </q-avatar>
+                  <div class="name">{{user.email}}</div>
+                </div>
+                <q-separator inset />
+                <div class="row no-wrap profile-options">
+                  <q-item clickable v-close-popup class="option" @click="logout">
+                    <q-item-section avatar style="min-width: 0">
+                      <q-icon name="exit_to_app" />
+                    </q-item-section>
+                    <q-item-section>Keluar</q-item-section>
+                  </q-item>
+                </div>
+              </div>
+            </q-menu>
+          </q-btn>
         </div>
       </q-toolbar>
     </q-header>
 
-    <q-drawer v-model="leftDrawerOpen" show-if-above bordered content-class="bg-grey-1">
-      <q-list>
-        <q-item-label header class="text-grey-8">Essential Links</q-item-label>
-        <EssentialLink v-for="link in essentialLinks" :key="link.title" v-bind="link" />
-      </q-list>
+    <q-drawer show-if-above bordered :width="250" content-class="bg-grey-0" v-model="leftDrawerOpen">
+      <q-scroll-area class="fit">
+        <q-list v-for="(menuItem, index) in menuList" :key="index">
+          <q-item clickable :to="menuItem.link" v-ripple>
+            <q-item-section avatar>
+              <q-icon :name="menuItem.icon" />
+            </q-item-section>
+            <q-item-section>{{ menuItem.label }}</q-item-section>
+          </q-item>
+
+          <q-separator v-if="menuItem.separator" />
+        </q-list>
+      </q-scroll-area>
     </q-drawer>
 
-    <q-page-container>
+    <!--<q-page-container>
       <router-view />
-    </q-page-container>
+    </q-page-container>-->
   </q-layout>
 </template>
 
 <script>
-import EssentialLink from "components/EssentialLink.vue";
-
-const linksData = [
-  {
-    title: "Customer",
-    caption: "customer list",
-    icon: "school",
-    link: "/customer",
-  },
-  {
-    title: "Telemarketer",
-    caption: "telemarketer admin",
-    icon: "school",
-    link: "/telemarketer",
-  },
-  {
-    title: "Admin Customer",
-    caption: "Admin customer list",
-    icon: "chat",
-    link: "/admin-customer",
-  },
-  // {
-  //   title: 'Assign New Customer',
-  //   caption: 'github.com/quasarframework',
-  //   icon: 'code',
-  //   link: 'https://github.com/quasarframework'
-  // },
-  // {
-  //   title: 'Discord Chat Channel',
-  //   caption: 'chat.quasar.dev',
-  //   icon: 'chat',
-  //   link: 'https://chat.quasar.dev'
-  // },
-  // {
-  //   title: 'Forum',
-  //   caption: 'forum.quasar.dev',
-  //   icon: 'record_voice_over',
-  //   link: 'https://forum.quasar.dev'
-  // },
-  // {
-  //   title: 'Twitter',
-  //   caption: '@quasarframework',
-  //   icon: 'rss_feed',
-  //   link: 'https://twitter.quasar.dev'
-  // },
-  // {
-  //   title: 'Facebook',
-  //   caption: '@QuasarFramework',
-  //   icon: 'public',
-  //   link: 'https://facebook.quasar.dev'
-  // },
-  // {
-  //   title: 'Quasar Awesome',
-  //   caption: 'Community Quasar projects',
-  //   icon: 'favorite',
-  //   link: 'https://awesome.quasar.dev'
-  // }
-];
-
 export default {
   name: "MainLayout",
-  components: { EssentialLink },
+  
   data() {
     return {
-      isLoggedIn: false,
-      userEmail: "",
       leftDrawerOpen: false,
-      essentialLinks: linksData,
+      menuList: [
+        {
+          icon: "dashboard",
+          label: "Dashboard",
+          separator: true,
+          link: { name: "dashboard" }
+        },
+        {
+          icon: "fas fa-user",
+          label: "Customer",
+          separator: false,
+          link: { name: "customer" }
+        },
+        {
+          icon: "fas fa-headset",
+          label: "Telemarketer",
+          separator: false,
+          link: { name: "telemarketer" }
+        },
+        {
+          icon: "fas fa-user-tie",
+          label: "Admin Customer",
+          separator: false,
+          link: { name: "admin-customer" }
+        }
+      ],
+      user:{},
     };
   },
   mounted() {
     var vm = this;
-
-    vm.isLoggedIn = vm.$authService.isTokenValid();
-    console.log("Loggedin", vm.isLoggedIn);
-    if (vm.isLoggedIn == true) {
-      // vm.userName = vm.$authService.getUserName();
-      var user = vm.$authService.getUser();
-      vm.userEmail = user.Email;
-      console.log(user, "ada user");
+    this.user = {
+      profilePic: vm.$authService.getProfilePicture(),
+      name: vm.$authService.getUserName(),
+      email: vm.$authService.getEmail()
+    }
+    // Dummy
+    this.user = {
+      profilePic: 'test',
+      name: 'Agung Kurniawan',
+      email: 'agung@fsn.co.id'
     }
   },
+
+  methods: {
+    logout() {
+      var vm = this;
+      vm.$authService.logout();
+      this.$router.replace({ name: 'login' })
+    },
+    onImageLoadFailure (event) {
+      event.target.src = require('assets/default.jpg')
+    }
+  }
 };
 </script>
--- frontend/src/router/routes.js
+++ frontend/src/router/routes.js
@@ -5,12 +5,11 @@ const routes = [
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/Index.vue') },
-      { path: '/dashboard', component: () => import('pages/Dashboard.vue') },
-
-      { path: '/customer', component: () => import('pages/Customer.vue') },
-      { path: '/telemarketer', component: () => import('pages/Telemarketer.vue') },
-      { path: '/admin-customer', component: () => import('pages/AdminCustomer.vue') },
-      { path: '/import-customer', component: () => import('pages/ImportCustomer.vue') },
+      { path: '/dashboard', name: 'dashboard', component: () => import('pages/Dashboard.vue') },
+      { path: '/customer', name: 'customer', component: () => import('pages/Customer.vue') },
+      { path: '/telemarketer', name: 'telemarketer', component: () => import('pages/Telemarketer.vue') },
+      { path: '/admin-customer', name: 'admin-customer', component: () => import('pages/AdminCustomer.vue') },
+      { path: '/import-customer', name: 'import-customer', component: () => import('pages/ImportCustomer.vue') },
     ],
     meta: {
       requiresAuth: false
