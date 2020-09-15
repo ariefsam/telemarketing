8b8e4a477c5a63c24a842f4c3975953d5627c53c
Arief Hidayatulloh <ariefhidayatulloh@gmail.com>
2020-09-15 17:12:46 +0700

    go mod init

---
+++ go.mod
@@ -0,0 +1,20 @@
+module github.com/ariefsam/telemarketing
+
+go 1.15
+
+require (
+	cloud.google.com/go/firestore v1.3.0
+	firebase.google.com/go v3.13.0+incompatible
+	github.com/360EntSecGroup-Skylar/excelize v1.4.1
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/gorilla/handlers v1.5.1
+	github.com/gorilla/mux v1.8.0
+	github.com/joho/godotenv v1.3.0
+	github.com/keimoon/gore v0.0.0-20160317032603-1ac8b93b5fdb
+	github.com/mitchellh/mapstructure v1.3.3
+	github.com/pkg/errors v0.9.1
+	github.com/stretchr/testify v1.4.0
+	github.com/teris-io/shortid v0.0.0-20171029131806-771a37caa5cf
+	google.golang.org/api v0.31.0
+	gopkg.in/webnice/b64.v1 v1.0.0
+)
--- restapi/ImportCustomer.go
+++ restapi/ImportCustomer.go
@@ -1,65 +1,61 @@
 package restapi
 
 import (
-	"log"
 	"net/http"
-
-	"github.com/360EntSecGroup-Skylar/excelize"
-	"github.com/ariefsam/telemarketing/entity"
 )
 
 func (api *RestAPI) ImportCustomer(w http.ResponseWriter, r *http.Request) {
 
-	keys := r.URL.Query()
+	// keys := r.URL.Query()
 
-	var token []string
-	var ok bool
-	if token, ok = keys["token"]; !ok {
-		return
-	}
-	var source []string
-	if source, ok = keys["source"]; !ok {
-		return
-	}
+	// var token []string
+	// var ok bool
+	// if token, ok = keys["token"]; !ok {
+	// 	return
+	// }
+	// var source []string
+	// if source, ok = keys["source"]; !ok {
+	// 	return
+	// }
 
-	post := RequestModel{
-		Token: &token[0],
-	}
-	telemarketer, err := api.authAdminOrResponseError(post, w)
-	if err != nil {
-		log.Println(err)
-		return
-	}
+	// post := RequestModel{
+	// 	Token: &token[0],
+	// }
+	// telemarketer, err := api.authAdminOrResponseError(post, w)
+	// if err != nil {
+	// 	log.Println(err)
+	// 	return
+	// }
 
-	r.ParseMultipartForm(10 << 20)
-	file, _, err := r.FormFile("file")
-	if err != nil {
-		return
-	}
-	defer file.Close()
-	f, err := excelize.OpenReader(file)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	rows, err := f.GetRows("Sheet1")
-	for idx, row := range rows {
-		if idx == 0 {
-			continue
-		}
-		customer := entity.Customer{
-			ID:               api.Usecase.GenerateID(),
-			PhoneNumber:      row[1],
-			Name:             row[0],
-			TimestampCreated: api.Usecase.CurrentTimestamp(),
-			CreatedBy:        telemarketer.ID,
-			DataSource:       source[0],
-		}
-		go func() {
-			err := api.Usecase.CreateCustomer(customer)
-			if err != nil {
-				log.Println(err)
-			}
-		}()
-	}
+	// r.ParseMultipartForm(10 << 20)
+	// file, _, err := r.FormFile("file")
+	// if err != nil {
+	// 	return
+	// }
+	// defer file.Close()
+	// f, err := excelize.OpenReader(file)
+	// if err != nil {
+	// 	log.Println(err)
+	// 	return
+	// }
+	// rows, err := f.GetRows("Sheet1")
+	// for idx, row := range rows {
+	// 	if idx == 0 {
+	// 		continue
+	// 	}
+	// 	customer := entity.Customer{
+	// 		ID:               api.Usecase.GenerateID(),
+	// 		PhoneNumber:      row[1],
+	// 		Name:             row[0],
+	// 		TimestampCreated: api.Usecase.CurrentTimestamp(),
+	// 		CreatedBy:        telemarketer.ID,
+	// 		DataSource:       source[0],
+	// 	}
+	// 	go func() {
+	// 		err := api.Usecase.CreateCustomer(customer)
+	// 		if err != nil {
+	// 			log.Println(err)
+	// 		}
+	// 	}()
+	// }
 }
